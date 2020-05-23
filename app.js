9f73915bf0116c0e358d79bbe0dbc84db05b47fe
Honza Kropacek <kropacek.h@gmail.com>
2020-05-23 19:10:07 +0200

    router deploy to routering the pages as a separate file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require ("express");
 const ejs = require ("ejs");
+
+const router = require("./router");
  
 const port = 3000;
 
@@ -8,9 +10,7 @@ myApp.use(express.static("public"));
 myApp.set("views", "views");
 myApp.set("view engine", "ejs");
 
-myApp.get("/", function(req, res){
-    res.render("home-quest");
-})
+myApp.use('/', router);
 
 myApp.listen(port, function(){
     console.log(`you are running on ${port} port`);
