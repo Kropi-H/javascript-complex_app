3bc037a998ede5d6a60209106d04bca621548660
Honza Kropacek <kropacek.h@gmail.com>
2020-05-26 21:09:47 +0200

    validate text input field

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,10 +7,13 @@ exports.logout = function () {};
 exports.register = function (req, res) {
   let user = new User(req.body); // This makes new instance of User = new object and seti it in to the variable user
   user.register();
-  res.send("Thanks for trying register");
+  if (user.errors.length) {
+    res.send(user.errors);
+  } else {
+    res.send("Congrats, there are no errors");
+  }
 };
 
 exports.home = function (req, res) {
   res.render("home-quest");
 };
- 
--- models/User.js
+++ models/User.js
@@ -1,9 +1,44 @@
-let User = function (data) {  // This is our CONSTRUCTOR FUNCTION. This is our reusable blueprint that can be used to create user objec tin other words we're going
+const validator = require("validator");
+let User = function (data) {
+  // This is our CONSTRUCTOR FUNCTION. This is our reusable blueprint that can be used to create user objec tin other words we're going
   this.data = data;
+  this.errors = [];
+};
+
+User.prototype.validate = function () {
+  if (this.data.username == "") {
+    this.errors.push("You must provide a username!");
+  }
+  if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)){
+     this.errors.push("Username can only contain letters and numbers!");
+  }
+  if (!validator.isEmail(this.data.email)) {
+    this.errors.push("You must provide a valid email!");
+  }
+  if (this.data.password == "") {
+    this.errors.push("You must provide a password!");
+  }
+  if (this.data.password.length > 0 && this.data.password.length < 12) {
+    this.errors.push("Password must be at least 12 characters!");
+  }
+  if (this.data.password.length > 100) {
+    this.errors.push("Password cannot exceed 100 characters!");
+  }
+  if (this.data.username.length > 0 && this.data.username.length < 3) {
+    this.errors.push("Username must be at least 3 characters!");
+  }
+  if (this.data.username.length > 30) {
+    this.errors.push("Username cannot exceed 30 characters!");
+  }
+
 };
 
 User.prototype.register = function () {
-     
+  //   Step #1: Validate user data
+  this.validate();
+
+  //   Step #2: Only if ther are no validation errors
+  //   then save the user data into a database
 };
 
 module.exports = User; // By this we can export our function to use it in other places we will calling from by using let User = require("./User")!
