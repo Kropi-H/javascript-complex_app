98b482b20c4eddb5943672df69c5b2c46294b9fb
Kropi <kropacek.h@gmail.com>
2020-05-31 20:32:17 +0200

    linux mint initialization commit

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const usersCollection = require("../db").collection("users");
 const validator = require("validator");
 let User = function (data) {
@@ -44,8 +45,8 @@ User.prototype.validate = function () {
   if (this.data.password.length > 0 && this.data.password.length < 12) {
     this.errors.push("Password must be at least 12 characters!");
   }
-  if (this.data.password.length > 100) {
-    this.errors.push("Password cannot exceed 100 characters!");
+  if (this.data.password.length > 50) {
+    this.errors.push("Password cannot exceed 50 characters!");
   }
   if (this.data.username.length > 0 && this.data.username.length < 3) {
     this.errors.push("Username must be at least 3 characters!");
@@ -59,7 +60,7 @@ User.prototype.login = function () {
   return new Promise( (resolve, reject) => {
     this.cleanUp();
     usersCollection.findOne({ username: this.data.username }).then((attemptedUser)=>{ // Mongodb returns promisses and we can use
-      if (attemptedUser && attemptedUser.password == this.data.password) {
+      if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
         resolve("Congrats!");
       } else {
         reject("Invalid username / password!");
@@ -78,6 +79,10 @@ User.prototype.register = function () {
   //   Step #2: Only if ther are no validation errors
   //   then save the user data into a database
   if (!this.errors.length) {
+
+    // Hash user passwodrd
+    let salt = bcrypt.genSaltSync(10);
+    this.data.password = bcrypt.hashSync(this.data.password, salt);
     usersCollection.insertOne(this.data);
   }
 };
