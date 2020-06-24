036279ee97058083dfdeeef2c56b6c210cf70438
Honza Kropacek <kropacek.h@gmail.com>
2020-06-24 18:03:13 +0200

    update package.json to the last version. Delete package-lock.json file and node_modules folder. Update .env file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,90 +1,82 @@
-const User = require("../models/User");
-const Post = require("../models/Post");
+const User = require('../models/User')
+const Post = require('../models/Post')
 
-exports.mustBeLoggedIn = function(req, res, next){
-  if(req.session.user){
+exports.mustBeLoggedIn = function(req, res, next) {
+  if (req.session.user) {
     next()
-  }else{
-  req.flash("errors", "You must be logged in to perform that action");
-    req.session.save(function(){
-      res.redirect('/');
+  } else {
+    req.flash("errors", "You must be logged in to perform that action.")
+    req.session.save(function() {
+      res.redirect('/')
     })
   }
 }
 
-
-exports.login = function (req, res) {
-  let user = new User(req.body);
-  user.login().then(function(result){ // If the promise is successfully
-    req.session.user = { // What we store to the database as session
-      avatar:user.avatar,
-      username:user.data.username,
-      _id:user.data._id
-    };
-    req.session.save(function(){
-      res.redirect('/');
-    });
-  }).catch(function(err){  // If the promise is unsuccessfully
-    req.flash('errors', err);
-    req.session.save(function(){
-      res.redirect('/');
-    })
-  });
-};
-
-exports.logout = function (req, res) {
-  req.session.destroy(function(){
-    res.redirect('/');
-  });
-};
-
-exports.register = function (req, res) {
-  let user = new User(req.body); // This makes new instance of User = new object and set it in to the variable user
-  user.register().then(()=>{
-    req.session.user = {
-      usename: user.data.username,
-      avatar: user.data.avatar,
-      _id:user.data._id
-    };
-    req.session.save(function(){
+exports.login = function(req, res) {
+  let user = new User(req.body)
+  user.login().then(function(result) {
+    req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
+    req.session.save(function() {
       res.redirect('/')
     })
-  }).catch((reqErrors)=>{
-    reqErrors.forEach(function(error){
+  }).catch(function(e) {
+    req.flash('errors', e)
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  })
+}
+
+exports.logout = function(req, res) {
+  req.session.destroy(function() {
+    res.redirect('/')
+  })
+}
+
+exports.register = function(req, res) {
+  let user = new User(req.body)
+  user.register().then(() => {
+    req.session.user = {username: user.data.username, avatar: user.avatar, _id: user.data._id}
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  }).catch((regErrors) => {
+    regErrors.forEach(function(error) {
       req.flash('regErrors', error)
     })
-    req.session.save(function(){
+    req.session.save(function() {
       res.redirect('/')
     })
-  });
-};
-
-exports.home = function (req, res) {
-  if(req.session.user){
-    res.render('home-dashboard');
-  }else{
-    res.render('home-quest', {regErrors: req.flash('regErrors')});
-  }
-};
-
-exports.ifUserExists = function(req, res, next){
-    User.findByUsername(req.params.username).then(function(userDocument){
-      req.profileUser = userDocument;
-      next();
-    }).catch(function(){
-      res.render('404');
-    });
+  })
 }
 
-exports.profilePostsScreen = function(req, res){
-  // Ask our post model for posts by a certain author id
-  Post.findByAuthorId(req.profileUser._id).then(function(posts){
-    res.render('profile-posts', {
+exports.home = function(req, res) {
+  if (req.session.user) {
+    res.render('home-dashboard')
+  } else {
+    res.render('home-guest', {regErrors: req.flash('regErrors')})
+  }
+}
+
+exports.ifUserExists = function(req, res, next) {
+  User.findByUsername(req.params.username).then(function(userDocument) {
+    req.profileUser = userDocument
+    next()
+  }).catch(function() {
+    res.render("404")
+  })
+}
+
+exports.profilePostsScreen = function(req, res) {
+  // ask our post model for posts by a certain author id
+  Post.findByAuthorId(req.profileUser._id).then(function(posts) {
+    res.render('profile', {
       posts: posts,
       profileUsername: req.profileUser.username,
       profileAvatar: req.profileUser.avatar
-    });
-  }).catch(function(){
-    res.render('404');
+    })
+  }).catch(function() {
+    res.render("404")
   })
+
 }
--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const mongodb = require("mongodb");
+const dotenv = require('dotenv')
+dotenv.config()
+const mongodb = require('mongodb')
 
-mongodb.connect(process.env.CONNECTIONSTRING,{useNewUrlParser:true, useUnifiedTopology:true},function(err, client){
-    module.exports = client;
-    const myApp = require('./app');
-    myApp.listen(process.env.PORT);
-});
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+  module.exports = client
+  const app = require('./app')
+  app.listen(process.env.PORT)
+})
